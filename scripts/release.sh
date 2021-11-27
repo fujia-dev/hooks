1bca41b7f4a025f25332c6b449623ffeeae589d6
fujia <fujia.site@outlook.com>
2021-11-27 15:52:14 +0800

    feat: [build] 0.1.0

--- scripts/release.sh
+++ scripts/release.sh
@@ -7,14 +7,14 @@ read -p "Releasing $VERSION - are you sure?(y/n)" -n 1 -r
 echo # move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-  sed -i "" 's/\("version": "\).*/\1'"$VERSION"'",/g' package.json
+  # sed -i "" 's/\("version": "\).*/\1'"$VERSION"'",/g' package.json
   echo "Relasing $VERSION ..."
 
   # upgrade version
   git add -A
-  git commit -m "feat: [build] $VERSION"
+  git commit --allow-empty -m "feat: [build] $VERSION"
   npm version $VERSION --message "feat: [release] $VERSION"
-  git push origin main
+  git push origin main --tags
 
   # publish
   npm publish --access public
